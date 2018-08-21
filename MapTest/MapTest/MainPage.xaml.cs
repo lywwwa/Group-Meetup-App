3b07627e7eb6640077c7b383e850a8508c40058b
ovvnzixl <ovvnzixl@gmail.com>
2018-08-21 22:31:09 +1000

    initial commit, needs new name

---
+++ MapTest/MapTest/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace MapTest
+{
+	public partial class MainPage : ContentPage
+	{
+
+        public MainPage()
+		{
+			InitializeComponent();
+        }
+
+        public void streetMap(object sender, EventArgs e)
+        {
+            mainMap.MapType = MapType.Street;
+        }
+
+        public void satelliteMap(object sender, EventArgs e)
+        {
+            mainMap.MapType = MapType.Satellite;
+        }
+
+        public void hybridMap(object sender, EventArgs e)
+        {
+            mainMap.MapType = MapType.Hybrid;
+        }
+
+
+    }
+}
